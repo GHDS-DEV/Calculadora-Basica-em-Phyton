b86c5019d69c9967d0387f27b68ed272411bb439
Gabriel Henrique <ghdsdev@gmail.com>
2025-09-03 23:27:10 -0300

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,23 @@
+num1 = 0
+num2 = 0
+result = 0
+op = ""
+
+while True:
+    num1 = float(input("Digite o primeiro número: "))
+    op = input("Digite a operação matemática ")
+    num2 = float(input("Digite o segundo número: "))
+
+    if   op == "+":
+        result = num1 + num2
+    elif op == "-":
+        result = num1 - num2
+    elif op == "/":
+        result = num1 / num2
+    elif op == "*":
+        result = num1 * num2
+        
+    else:
+        print("Operação inválida!")
+
+    print("{} {} {} = {}".format(num1, op, num2, result))
